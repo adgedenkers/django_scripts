acc5000593a09cbc0be351696e591a25a00970fa
Adge Denkers <adge.denkers@gmail.com>
2023-02-23 12:38:08 -0500

    added ascii art

--- setup_django.sh
+++ setup_django.sh
@@ -4,13 +4,41 @@ echo "author:  Adge Denkers / adge.denkers@gmail.com"
 echo "updated: 2023-01-06"
 echo "----------------------------------------------"
 echo ""
-echo "Please enter the IP address:"
-read ip
-#ip="18.215.211.25"
+#          _      
+#      ___| |__   
+#     / __| '_ \  
+#    _\__ \ | | | 
+#   (_)___/_| |_| 
+#    ______ ______ ______ ______ ______  
+#   |______|______|______|______|______| 
+#
+#    _   _ ___  ___ _ __     
+#   | | | / __|/ _ \ '__|    
+#   | |_| \__ \  __/ |       
+#    \__,_|___/\___|_|       
+#    _                   _   
+#   (_)_ __  _ __  _   _| |_ 
+#   | | '_ \| '_ \| | | | __|
+#   | | | | | |_) | |_| | |_ 
+#   |_|_| |_| .__/ \__,_|\__|
+#           |_|              
+
+echo "Only proceed if you have already created a new github repository for your project, and have that repository name on hand."
+
+echo "denkers.co website ip is: 18.215.211.25"
+echo
+ip=read("Please enter the server IP address:")
+# read ip
+#
 
 app_name=read("Please enter the name of the app (no spaces or hyphens): ")
 git_repo=read("Please enter the git repository for the app (no spaces or hyphens): ")
 
+#    _           _        _ _  
+#   (_)_ __  ___| |_ __ _| | | 
+#   | | '_ \/ __| __/ _` | | | 
+#   | | | | \__ \ || (_| | | | 
+#   |_|_| |_|___/\__\__,_|_|_| 
 
 # switch to user directory
 cd ~
@@ -22,6 +50,19 @@ sudo apt update
 sudo apt install python3-pip python3-venv -y
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11
 sudo apt install python3-django -y
+
+
+#             _                       
+#    ___  ___| |_ _   _ _ __          
+#   / __|/ _ \ __| | | | '_ \         
+#   \__ \  __/ |_| |_| | |_) |        
+#   |___/\___|\__|\__,_| .__/         
+#        _  _          |_|            
+#     __| |(_) __ _ _ __   __ _  ___  
+#    / _` || |/ _` | '_ \ / _` |/ _ \ 
+#   | (_| || | (_| | | | | (_| | (_) |
+#    \__,_|/ |\__,_|_| |_|\__, |\___/ 
+#        |__/             |___/       
 
 # create a projects directory
 mkdir projects || echo "projects directory already exists, moving on..."
@@ -42,6 +83,24 @@ python configure_settings.py "$ip"
 # setup and activate the python virtual environment
 python -m venv venv
 source venv/bin/activate
+
+#             _                   
+#    ___  ___| |_ _   _ _ __      
+#   / __|/ _ \ __| | | | '_ \     
+#   \__ \  __/ |_| |_| | |_) |    
+#   |___/\___|\__|\__,_| .__/     
+#                 _    |_|        
+#   __      _____| |__            
+#   \ \ /\ / / _ \ '_ \           
+#    \ V  V /  __/ |_) |          
+#     \_/\_/ \___|_.__/           
+#    ___  ___ _ ____   _____ _ __ 
+#   / __|/ _ \ '__\ \ / / _ \ '__|
+#   \__ \  __/ |   \ V /  __/ |   
+#   |___/\___|_|    \_/ \___|_|   
+#                                 
+# figure out what web server to use
+# ---------------------------------------------
 
 # install and setup nginx
 sudo apt install nginx -y
