8a843e29d633e7b8ee37c74806a8609eaa90cbc3
Adge Denkers <adge.denkers@gmail.com>
2023-02-23 09:18:18 -0500

    updated setup_django.sh

--- setup_django.sh
+++ setup_django.sh
@@ -8,6 +8,10 @@ echo "Please enter the IP address:"
 read ip
 #ip="18.215.211.25"
 
+app_name=read("Please enter the name of the app (no spaces or hyphens): ")
+git_repo=read("Please enter the git repository for the app (no spaces or hyphens): ")
+
+
 # switch to user directory
 cd ~
 
@@ -16,11 +20,11 @@ sudo apt update
 
 # install python3 and django
 sudo apt install python3-pip python3-venv -y
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.11
 sudo apt install python3-django -y
 
 # create a projects directory
-mkdir projects || echo "projects directory already exists"
+mkdir projects || echo "projects directory already exists, moving on..."
 
 cd projects
 
@@ -28,8 +32,8 @@ cd projects
 django-admin startproject django_app
 
 # rename the root folder of the django_app to www
-mv django_app www
-cd www
+mv django_app $app_name
+cd $app_name
 
 # modify the allowed hosts, adding the IP address of the server
 curl -O https://raw.githubusercontent.com/adgedenkers/django_scripts/main/configure_settings.py
@@ -44,7 +48,8 @@ sudo apt install nginx -y
 
 touch django_app_nginx
 echo -e "server { \nlisten 80; \nserver_name "$ip";\nlocation / {\n include proxy_params;\n proxy_pass http://localhost:8000/;\n}\n}" >> django_app_nginx
-sudo mv django_app_nginx /etc/nginx/sites-available/django_app
+sudo mv django_app_nginx /etc/nginx/sites-available/django_app:pe
+
 sudo chmod 644 /etc/nginx/sites-available/django_app
 sudo ln -s /etc/nginx/sites-available/django-app /etc/nginx/sites-enabled
 
