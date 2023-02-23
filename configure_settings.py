4a718e3223393fd940806e02b0f73b5b9df6a3e7
Adge Denkers <adge.denkers@gmail.com>
2023-02-23 12:43:30 -0500

    removed duplicate code

--- configure_settings.py
+++ configure_settings.py
@@ -1,28 +1,25 @@
 # ----------------------------------------------
 # author:  Adge Denkers / adge.denkers@gmail.com
-# updated: 2023-01-06
+# updated: 2023-02-23
 # ----------------------------------------------
 import sys
 
 for i in range(1, len(sys.argv)):
     ip = sys.argv[i]
 
-search_text = "ALLOWED_HOSTS = []"
-replace_text = "ALLOWED_HOSTS = ['{}']".format(ip)
+
 
 with open(r'/home/ubuntu/projects/www/django_app/settings.py', 'r') as file:
     data = file.read()
+
+    # replace allowed hosts
+    search_text = "ALLOWED_HOSTS = []"
+    replace_text = "ALLOWED_HOSTS = ['{}']".format(ip)
     data = data.replace(search_text, replace_text)
 
-with open(r'/home/ubuntu/projects/www/django_app/settings.py', 'w') as file:
-    file.write(data)
-
-# now replace the time zone info
-search_text = "TIME_ZONE = 'UTC'"
-replace_text = "TIME_ZONE = 'America/New_York'"
-
-with open(r'/home/ubuntu/projects/www/django_app/settings.py', 'r') as file:
-    data = file.read()
+    # now replace the time zone info
+    search_text = "TIME_ZONE = 'UTC'"
+    replace_text = "TIME_ZONE = 'America/New_York'"
     data = data.replace(search_text, replace_text)
 
 with open(r'/home/ubuntu/projects/www/django_app/settings.py', 'w') as file:
